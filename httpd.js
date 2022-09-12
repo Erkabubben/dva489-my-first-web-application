b7f6f9643e2a169b21e45fd93bcfed0ca6161f93
Erik Lindholm <elimk06@student.lnu.se>
2022-09-12 16:58:32 +0200

    Successfully serves "Hello World!" as HTML

--- httpd.js
+++ httpd.js
@@ -2,10 +2,9 @@ var http = require('http');
 
 var server = http.createServer((req, res) => {
   console.log(req.method + " " + req.url + " " + req.httpVersion);
-  res.statusCode(200)
-  res.setHeader("Content-Type", "text/html")
-  //res.send('<p>Hello World!</p>')
-  //res.end();
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello World!</h1>')
+  res.end();
 });
 
 server.listen(8000);
