4875bea28c1a90a06fc7a4b5b6f4d1d7472684f1
Erik Lindholm <elimk06@student.lnu.se>
2022-09-12 20:06:30 +0200

    Successfully serves CSS-styled static files

--- httpd.js
+++ httpd.js
@@ -1,10 +1,33 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer((req, res) => {
-  console.log(req.method + " " + req.url + " " + req.httpVersion);
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello World!</h1>')
-  res.end();
+    console.log(req.method + " " + req.url + " " + req.httpVersion);
+
+    var contentTypes = {
+        '.html': 'text/html',
+        '.css': "text/css",
+        '.js': 'application/javascript'
+    };
+
+    fs.readFile('public' + req.url, function(err, data) {
+        if (err) {
+            res.writeHead(404);
+            return res.end("File not found.");
+        }
+
+    if (req.url.endsWith('.html')) {
+        res.setHeader("Content-Type", 'text/html');
+    }
+    else if (req.url.endsWith('.css')) {
+        res.setHeader("Content-Type", 'text/css');
+    }
+    else if (req.url.endsWith('.css')) {
+        res.setHeader("Content-Type", 'text/javascript');
+    }
+    res.writeHead(200);
+    res.end(data);
+    });
 });
 
 server.listen(8000);
